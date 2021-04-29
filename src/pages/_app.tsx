0eaad1bf98600f0063c6f6855a73a7c99287132f
Miguel Nogueira <mglsndc@gmail.com>
2021-04-29 09:28:10 -0300

    wrap _app with auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/global.css';
+import { AuthProvider } from '../contexts/AuthContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
