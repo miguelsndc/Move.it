1fd19e9b0def27cc272bb11640610e016253f2a5
Miguel Nogueira <mglsndc@gmail.com>
2021-04-29 09:26:48 -0300

    add loading spinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/components/LoadingSpinner.module.css';
+
+function LoadingSpinner() {
+  return <div className={styles.spinner}></div>;
+}
+
+export default LoadingSpinner;
