a948194e235f9e04b499f2da6d61440f943b48d3
Miguel Nogueira <mglsndc@gmail.com>
2021-04-29 17:04:48 -0300

    add login form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/pages/Login.module.css';
+import { SignWithProviderButton } from '../../components/SignInWithProvider';
+import { InputField } from '../../components/InputField';
 import { useAuth } from '../../contexts/AuthContext';
 
-export default function LoginPage() {
+export default function Login() {
   const {
     signInWithGithub,
     signInWithGoogle,
@@ -9,5 +14,48 @@ export default function LoginPage() {
     user,
   } = useAuth();
 
-  return <div>Login</div>;
+  return (
+    <>
+      <Head>
+        <title>Login | Move.it</title>
+      </Head>{' '}
+      <div className={styles.container}>
+        <form className={styles.formGroup}>
+          <h1>Entrar</h1>
+          <SignWithProviderButton imagePath="/icons/google.svg">
+            Entrar com Google
+          </SignWithProviderButton>
+          <SignWithProviderButton imagePath="/icons/github.svg">
+            Entrar com Google
+          </SignWithProviderButton>
+          <hr />
+          <div>
+            <label htmlFor="email">Email</label>
+            <InputField
+              type="email"
+              name="email"
+              placeholder="Digite seu E-mail"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Senha</label>
+            <InputField
+              name="password"
+              placeholder="Digite sua senha"
+              type="password"
+            />
+          </div>
+          <hr />
+
+          <div>
+            Ainda não tem uma conta ?{' '}
+            <Link href="/signup">
+              <a>Cadastre-se</a>
+            </Link>
+          </div>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </>
+  );
 }
