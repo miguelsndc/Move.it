07ce319e911eb12c2ff37450748fcf49af9967f8
Miguel Nogueira <mglsndc@gmail.com>
2021-04-30 15:13:08 -0300

    creating unique styled c file for shared styles amongst pages

---
+++ src/styles/pages/Shared.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ErrorWarning = styled.div`
+  color: var(--red);
+  margin: 0.5rem 0;
+`;
