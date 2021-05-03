1ef7e9d32349d9159561dbec87c533ab89aa57ed
Miguel Nogueira <mglsndc@gmail.com>
2021-05-03 11:29:26 -0300

    add sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+import { IconContext } from 'react-icons';
+import { GoGear } from 'react-icons/go';
+import { IoPodium } from 'react-icons/io5';
+import { FaUser } from 'react-icons/fa';
+
+import { SidebarContainer } from './styles';
+
+export function Sidebar() {
+  return (
+    <SidebarContainer>
+      <IconContext.Provider value={{ color: '#5965e0', size: '2rem' }}>
+        <div>
+          <Link href="/">
+            <a>
+              <FaUser />
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/ranking">
+            <a>
+              <IoPodium />
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/config">
+            <a>
+              <GoGear />
+            </a>
+          </Link>
+        </div>
+      </IconContext.Provider>
+    </SidebarContainer>
+  );
+}
---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SidebarContainer = styled.aside`
+  height: 100vh;
+  width: 6.5rem;
+  background: #fff;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  div {
+    margin: 1rem 0;
+  }
+`;
