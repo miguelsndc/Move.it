bc03d76cb3e4a0b5a99ef40693c9e805f6ea2f3c
Miguel Nogueira <mglsndc@gmail.com>
2021-05-06 11:10:36 -0300

    add light and dark theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,19 @@
+const darkTheme = {
+  title: 'dark',
+
+  colors: {
+    white: '#454545',
+    background: '#2b2b2b',
+    grayLine: '#595959',
+    text: '#666',
+    textHighlight: '#b3b9ff',
+    title: '#ebebeb',
+    red: '#e83f5b',
+    green: '#4cd62b',
+    blue: '#5965e0',
+    blueDark: '#4953b8',
+    blueTwitter: '#2aa9e0',
+  },
+};
+
+export default darkTheme;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,19 @@
+const lightTheme = {
+  title: 'light',
+
+  colors: {
+    white: '#fff',
+    background: '#f2f3f5',
+    grayLine: '#dcdde0',
+    text: '#666',
+    textHighlight: '#b3b9ff',
+    title: '#2e384d',
+    red: '#e83f5b',
+    green: '#4cd62b',
+    blue: '#5965e0',
+    blueDark: '#4953b8',
+    blueTwitter: '#2aa9e0',
+  },
+};
+
+export default lightTheme;
