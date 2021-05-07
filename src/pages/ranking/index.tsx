a9e8957d49f69a8f52c0d4f46563796d2f9d1790
Miguel Nogueira <mglsndc@gmail.com>
2021-05-07 12:09:41 -0300

    add ternary check if user is logged and add next ssg revalidation

--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -1,105 +1,56 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import withAuth from '../../components/auth/WithAuth'
-import { Spinner } from '../../components/LoadingSpinner/styles'
-import Sidebar from '../../components/Sidebar'
+import { GetStaticProps } from 'next'
 import { db } from '../../config/firebase'
-import {
-  Container,
-  Rank,
-  InfoWrapper,
-  UserInfo,
-  Position,
-  Profile,
-  Experience,
-} from '../../styles/Ranking'
+import { useAuth } from '../../contexts/AuthContext'
+import RankingTemplate from '../../templates/RankingTemplate'
+import { redirectTo } from '../../utils/redirectTo'
 
-interface User {
-  id: string
+interface UserProps {
   ChallengesCompleted: number
   CurrentExperience: number
   Level: number
   PhotoUrl: string
+  id: string
   name: string
 }
+interface IRankingProps {
+  users: UserProps[]
+}
 
-function Ranking(HomeProps: any) {
-  console.log(HomeProps)
-  const usersRef = db.collection('users')
-  const [docs, setDocs] = useState<User[]>()
-  const [loading, setLoading] = useState(true)
-
-  async function sortUsersByChallengesCompleted() {
-    const snapshot = usersRef.orderBy('ChallengesCompleted').get()
-    const descendingOrderSnapshot: User[] = []
-    ;(await snapshot).docs.forEach((doc) => {
-      const {
-        id,
-        ChallengesCompleted,
-        CurrentExperience,
-        Level,
-        PhotoUrl,
-        name,
-      } = doc.data()
-      descendingOrderSnapshot.push({
-        id,
-        ChallengesCompleted,
-        CurrentExperience,
-        Level,
-        PhotoUrl,
-        name,
-      })
-    })
-    descendingOrderSnapshot.reverse()
-    setDocs(descendingOrderSnapshot)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    sortUsersByChallengesCompleted()
-  }, [])
-
+function Ranking(RankingProps: IRankingProps) {
+  const { isLoggedIn } = useAuth()
   return (
-    <main style={{ display: 'flex' }}>
-      <Sidebar />
-      {loading ? (
-        <Spinner />
+    <>
+      {isLoggedIn ? (
+        <RankingTemplate {...RankingProps} />
       ) : (
-        <Container>
-          <h1>Leaderboard</h1>
-          <Rank>
-            {docs.map((doc, index) => {
-              return (
-                <InfoWrapper key={doc.id}>
-                  <Position>{index + 1}</Position>
-                  <UserInfo>
-                    <Profile>
-                      <img src="/diver.svg" alt="Foto" />
-                      <div>
-                        <strong>{doc.name}</strong>
-                        <p>Nível {doc.Level}</p>
-                      </div>
-                    </Profile>
-                    <Experience>
-                      <div>
-                        <strong>{doc.ChallengesCompleted}</strong>
-                        <p>completados</p>
-                      </div>
-                      <div>
-                        <strong>{doc.CurrentExperience}</strong>
-                        <p>XP</p>
-                      </div>
-                    </Experience>
-                  </UserInfo>
-                </InfoWrapper>
-              )
-            })}
-          </Rank>
-        </Container>
+        redirectTo('/login')
       )}
-    </main>
+    </>
   )
 }
 
-export default withAuth(Ranking)
+export default Ranking
+
+export const getStaticProps: GetStaticProps = async () => {
+  const users = []
+
+  try {
+    const querySnapshot = await db
+      .collection('users')
+      .orderBy('ChallengesCompleted')
+      .get()
+
+    querySnapshot.forEach((doc) => {
+      users.push(doc.data())
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  return {
+    props: {
+      users,
+    },
+    revalidate: 90,
+  }
+}
