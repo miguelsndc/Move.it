a92e84fa154b30b759102f5f5c799e76167e7303
Miguel Nogueira <mglsndc@gmail.com>
2021-05-10 17:06:23 -0300

    add loading to await user data fetching and add config template

--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -1,25 +1,44 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Sidebar } from '../../components/Sidebar'
+import { db } from '../../config/firebase'
 import { useAuth } from '../../contexts/AuthContext'
+import ConfigTemplate from '../../templates/ConfigTemplate'
 
 function Config() {
-  const { signOut, isLoggedIn } = useAuth()
+  const { signOut, isLoggedIn, user } = useAuth()
   const router = useRouter()
 
   if (!isLoggedIn) {
     router.push('/login')
   }
 
+  const [currentUserData, setCurrentUserData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users')
+        .doc(user.uid)
+        .get()
+        .then((doc) => {
+          setCurrentUserData(doc.data())
+          setLoading(false)
+        })
+    }
+  }, [])
+
   return (
     <>
-      {isLoggedIn && (
-        <main style={{ display: 'flex' }}>
-          <Sidebar />
-          <div style={{ margin: '0 auto' }}>
-            <button onClick={signOut}>sair</button>
-          </div>
-        </main>
+      {loading || (
+        <>
+          {isLoggedIn && (
+            <main style={{ display: 'flex' }}>
+              <Sidebar />
+              <ConfigTemplate {...currentUserData} signOut={signOut} />
+            </main>
+          )}
+        </>
       )}
     </>
   )
